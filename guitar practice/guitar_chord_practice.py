ba21bb64d857394f4c2077993c57c14757ef6d5e
DaylanDStoica <daylanstoica@gmail.com>
2023-10-08 17:28:01 -0700

    completed the guitar chord list, and added a paramater to add for user key between chord requests

--- guitar practice/guitar_chord_practice.py
+++ guitar practice/guitar_chord_practice.py
@@ -3,15 +3,18 @@
 
 import random
 from time import sleep
-def random_chord_holds( time_gaps = 1.0 ):
+def random_chord_holds( time_gaps = 1.0 , wait_for_input = False):
     # every X seconds (time_gaps), print a new chord to play
     # repeat until exit program, Ctrl+Q
-    chords = ["C", "Dm", "C sharp"]
+    chords = "E A D C G E_minor A_minor D_minor C7 G7 E7 A7 D7 F B7".split()
+    print(" running from the chord list of : ")
+    print("         ", chords)
     while ( True):
         sleep(time_gaps)
-
+        if wait_for_input: # wait for user input before generating next chord
+            input("Button press when ready: ")
         rand_chord = random.choice(chords)
         print( rand_chord, " chord" )
 
 
-random_chord_holds(2)
+random_chord_holds(2, True)
