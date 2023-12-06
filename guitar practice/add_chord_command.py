7d937efca994fa81ba08a1c1a6199bfc7c124623
DaylanDStoica <daylanstoica@gmail.com>
2023-12-05 21:07:46 -0800

    fixed the loop for adding chord names and string values. No longer crashes when presented an empty string

--- guitar practice/add_chord_command.py
+++ guitar practice/add_chord_command.py
@@ -12,13 +12,20 @@ def addChordToFile( chord_and_fret_string):
     chord_name, fret_placement = chord_and_fret_string.split() 
     in_string = chord_name + " = " + fret_placement
     
-    file_access.write(in_string) 
-    
+    # file_access.write(in_string) 
+    file_access.write(in_string + '\n')
     
 def loopAddChords():
-    chord_and_fret = input("chord_name fret: ")
+    # chord_and_fret = input("chord_name fret: ")
+    chord_and_fret = " "
     while( chord_and_fret != ""): # while the string is not empty
+        chord_and_fret = input("chord_name, fret: ")
+        if chord_and_fret == "" : # if the input is detected as empty, exit
+            break 
+        elif " " not in chord_and_fret: # if there is not a space, split() cannot work 
+            break
         addChordToFile(chord_and_fret)
+        
         
     file_access.close()
     
